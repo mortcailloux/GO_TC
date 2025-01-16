c2ed33f356d105bee995acfc09e4dbf8d7306d3f
cb <charlesbouquet1011@gmail.com>
2025-01-16 11:29:35 +0100

    chaque goroutine fait maintenant plus qu'une seule case (ce n'était pas efficace)

--- cellule.go
+++ cellule.go
@@ -3,63 +3,85 @@ package main
 import (
 	"math/rand"
 	"sync"
-	"time"
 )
 
 // Cell represents a single individual in the grid
 
-// evolveCell evolves the state of a cell based on its neighbors
-func evolveCell(cell *Cell, grid [][]Cell, wg *sync.WaitGroup, syncmodification *sync.WaitGroup, changement *bool) {
-	rand.Seed(time.Now().UnixNano())
+// Calcule le prochain état d'une cellule sans modifier la cellule originale, renvoie une nouvelle Cellule correspondant à la cellule modifiée
+func evolveCell(currentCell Cell, neighbors []Cell) Cell {
 
-	// Find neighboring cells
-	neighbors := findNeighbors(cell, grid)
+	newCell := currentCell.clone()
 
-	// For each possible state
-	switch cell.Etat {
-	case "S": // Susceptible
-		infectedNeighbors := countInfectedNeighbors(neighbors)
-
-		// Calculate infection probability
-		// infectionProbability = base infection chance * number of infected neighbors / total neighbors
-		proba := cell.probaInfectionMoyenne * float32(infectedNeighbors) / float32(len(neighbors))
-
-		// Infection only happens if the random number is less than the infection probability
-		syncmodification.Done()
-		syncmodification.Wait()
+	switch currentCell.Etat {
+	case "S":
+		infectedCount := 0
+		for _, n := range neighbors {
+			if n.Etat == "I" {
+				infectedCount++
+			}
+		}
+		proba := currentCell.probaInfectionMoyenne * float32(infectedCount) / float32(len(neighbors))
 		if rand.Float32() < proba {
-
-			*changement = true
-			cell.Etat = "I"
-			cell.tempsInfectionRestant = rand.Intn(5) + 1 // Random infection duration (e.g., 1-5 iterations)
+			newCell.Etat = "I"
+			newCell.tempsInfectionRestant = rand.Intn(5) + 1
 		}
-	case "I": // Infected
-		cell.tempsInfectionRestant--
-		syncmodification.Done()
-		syncmodification.Wait()
-		if cell.tempsInfectionRestant <= 0 {
-
-			*changement = true
-			cell.Etat = "G"
-			cell.tempsImmuniteRestant = rand.Intn(5) + 1 // Random immunity duration (e.g., 1-5 iterations)
+	case "I":
+		newCell.tempsInfectionRestant--
+		if newCell.tempsInfectionRestant <= 0 {
+			newCell.Etat = "G"
+			newCell.tempsImmuniteRestant = rand.Intn(5) + 1
 		}
-	case "G": // Recovered (immune)
-		syncmodification.Done()
-		syncmodification.Wait()
-		cell.tempsImmuniteRestant--
-
-		if cell.tempsImmuniteRestant <= 0 {
-
-			*changement = true
-			cell.Etat = "S" // Becomes susceptible again
+	case "G":
+		newCell.tempsImmuniteRestant--
+		if newCell.tempsImmuniteRestant <= 0 {
+			newCell.Etat = "S"
 		}
 	}
-	wg.Done()
-	//utile pour arrêter le programme plus tôt s'il n'y a plus de changements même si les itérations n'ont pas fini
+	return newCell
 }
 
-// findNeighbors finds the neighbors of a cell in the grid
-func findNeighbors(cell *Cell, grid [][]Cell) []Cell {
+func findNeighbors(cell *Cell, grid [][]Cell, size int) []Cell {
+	directions := []struct{ di, dj int }{
+		{-1, 0}, {1, 0}, {0, -1}, {0, 1},
+		{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
+	}
+
+	neighbors := make([]Cell, 0, 8)
+	for _, d := range directions {
+		ni, nj := cell.posi+d.di, cell.posj+d.dj
+		if ni >= 0 && ni < size && nj >= 0 && nj < size {
+			neighbors = append(neighbors, grid[ni][nj])
+		}
+	}
+	return neighbors
+}
+
+func processBatch(currentGrid, nextGrid [][]Cell, start, end, size int, changement *bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := start; i < end; i++ {
+		row := i / size //on va parcourir notre nombre de cellules du tableau de gauche à droite puis de haut en bas
+		//cellule 0 par exemple avec, cela donne ligne 0 et col 0, cellule 1, ligne0, col 1 (puisque / est le quotient et % le reste)
+		//c'est un peu des maths mais ça fonctionne
+		col := i % size
+
+		// Lecture des voisins depuis la grille de lecture (on écrit dans la 2e pour éviter les conflits)
+		neighbors := findNeighbors(&currentGrid[row][col], currentGrid, size)
+
+		// Calcul du nouvel état
+		newCell := evolveCell(currentGrid[row][col], neighbors)
+
+		// Si l'état a changé, on le note
+		if newCell.Etat != currentGrid[row][col].Etat {
+			*changement = true
+		}
+		//la variable changement ne fonctionnait pas puisqu'elle est accédée par de nombreux processus, le programme ne fonctonnait pas correctement,
+		//à voir si ça vaut le coup de patch
+		// Écriture dans la nouvelle grille (grille d'écriture)
+		nextGrid[row][col] = newCell
+	}
+}
+func findNeighbors1(cell *Cell, grid [][]Cell) []Cell {
 	directions := []struct{ di, dj int }{
 		{-1, 0}, {1, 0}, {0, -1}, {0, 1}, // Cardinal directions (up, down, left, right)
 		{-1, -1}, {-1, 1}, {1, -1}, {1, 1}, // Diagonal directions
